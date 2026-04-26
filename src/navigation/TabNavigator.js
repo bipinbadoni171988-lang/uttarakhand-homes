d96588e94d5ba0c16e06f36cc744543b53d782f7
bipinbadoni171988-lang <bipinbadoni171988@gmail.com>
2026-04-26 17:00:00 +0530

    Refactor TabNavigator to MainNavigator with Stack

--- src/navigation/TabNavigator.js
+++ src/navigation/TabNavigator.js
@@ -1,78 +1,20 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// This file (or App.js) should be updated to add the screen in your navigator stack.
 
-import HomeScreen from '../screens/HomeScreen';
-import PropertyScreen from '../screens/PropertyScreen';
-import RentalsScreen from '../screens/RentalsScreen';
-import ServicesScreen from '../screens/ServicesScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import { TEAL } from '../constants/theme';
+import { createStackNavigator } from '@react-navigation/stack';
+import PhoneEntryScreen from '../screens/PhoneEntryScreen';
+import OtpScreen from '../screens/OtpScreen'; // You must create this as the OTP entry screen.
 
-const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-const TabIcon = ({ icon }) => <Text style={{ fontSize: 22 }}>{icon}</Text>;
-
-const ProfileWrapper = ({ user, userData, role, onLogout }) => (
-  <ProfileScreen user={user} userData={userData} role={role} onLogout={onLogout} />
-);
-
-export const TabNavigator = ({ user, userData, role, onLogout }) => {
+export default function MainNavigator() {
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: TEAL,
-        tabBarInactiveTintColor: '#999',
-        tabBarStyle: {
-          backgroundColor: 'white',
-          height: 62,
-          paddingBottom: 8,
-          paddingTop: 6,
-          borderTopColor: '#eee',
-        },
-        tabBarLabelStyle: {
-          fontSize: 11,
-          fontWeight: '700',
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{ tabBarIcon: () => <TabIcon icon="🏡" /> }}
-      />
-      <Tab.Screen
-        name="Properties"
-        component={PropertyScreen}
-        options={{ tabBarIcon: () => <TabIcon icon="🏠" /> }}
-      />
-      <Tab.Screen
-        name="Rentals"
-        component={RentalsScreen}
-        options={{ tabBarIcon: () => <TabIcon icon="📦" /> }}
-      />
-      <Tab.Screen
-        name="Services"
-        component={ServicesScreen}
-        options={{ tabBarIcon: () => <TabIcon icon="🔧" /> }}
-      />
-      <Tab.Screen
-        name="Profile"
-        options={{ tabBarIcon: () => <TabIcon icon="👤" /> }}
-      >
-        {() => (
-          <ProfileWrapper
-            user={user}
-            userData={userData}
-            role={role}
-            onLogout={onLogout}
-          />
-        )}
-      </Tab.Screen>
-    </Tab.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="PhoneEntry" component={PhoneEntryScreen} />
+      <Stack.Screen name="OtpScreen" component={OtpScreen} />
+      {/* ...other screens/tabs as before */}
+      {/* If you have guest mode, Home and others should still be accessible */}
+    </Stack.Navigator>
   );
-};
+}
 
-export default TabNavigator;
-// TODO: implement
+// Else, if your root is declared in App.js, add <Stack.Screen ... /> for PhoneEntry and OtpScreen there.
